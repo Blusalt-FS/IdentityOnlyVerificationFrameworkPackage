e079fe0af9927667e9e7f9223449a17b09d07c47
Folarin <opefolarin1@gmail.com>
2024-03-01 23:01:01 +0000

    Initial Commit

---
+++ Sources/IdentityOnlyVerificationFramework.xcframework/ios-arm64/IdentityOnlyVerificationFramework.framework/Headers/IdentityOnlyVerificationFramework.h
@@ -0,0 +1,18 @@
+//
+//  IdentityOnlyVerificationFramework.h
+//  IdentityOnlyVerificationFramework
+//
+//  Created by Folarin Opeyemi on 09/01/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IdentityOnlyVerificationFramework.
+FOUNDATION_EXPORT double IdentityOnlyVerificationFrameworkVersionNumber;
+
+//! Project version string for IdentityOnlyVerificationFramework.
+FOUNDATION_EXPORT const unsigned char IdentityOnlyVerificationFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IdentityOnlyVerificationFramework/PublicHeader.h>
+
+
